99f9187f1f0ae6a34f0bc4b48f05aaab38f1faa3
Item135 <alfredyap57@yahoo.com.sg>
2026-03-16 20:53:04 +0800

    feat: Scaffold update and Seeding

--- Domain/Entities/Analytic.cs
+++ Domain/Entities/Analytic.cs
@@ -15,13 +15,11 @@ public partial class Analytic
 
     public int? Returnamt { get; private set; }
 
-    public string? Primarysupplier { get; private set; }
+    public int? Refprimaryid { get; private set; }
 
-    public string? Primaryitem { get; private set; }
+    public string? Refprimaryname { get; private set; }
 
-    public decimal? Supplierreliability { get; private set; }
-
-    public decimal? Turnoverrate { get; private set; }
+    public decimal? Refvalue { get; private set; }
 
     public virtual ICollection<Reportexport> Reportexports { get; private set; } = new List<Reportexport>();
 

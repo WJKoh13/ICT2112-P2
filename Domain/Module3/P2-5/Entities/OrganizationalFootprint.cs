bff77d94566119608269ce68350e80273d4bfbff
ChohKaifeng <chohkfeng32@gmail.com>
2026-03-23 01:58:03 +0800

    Implemented Strategies

--- Domain/Module3/P2-5/Entities/OrganizationalFootprint.cs
+++ Domain/Module3/P2-5/Entities/OrganizationalFootprint.cs
@@ -2,16 +2,38 @@ namespace ProRental.Domain.Entities;
 
 public class OrganizationalFootprint
 {
-    public string OrganizationId { get; set; }
-    public string OrganizationName { get; set; }
-    public float Volume { get; set; }
-    public float ToxicPercentage { get; set; }
+    private string _organizationalFootprintId;
+    private string _organizationalId;
+    private string _organizationName;
+    private float _volume;
+    private float _toxicPercentage;
 
     public OrganizationalFootprint(string organizationId, string organizationName, float volume, float toxicPercentage)
     {
-        OrganizationId = organizationId;
-        OrganizationName = organizationName;
-        Volume = volume;
-        ToxicPercentage = toxicPercentage;
+        _organizationalFootprintId = Guid.NewGuid().ToString();
+        _organizationalId = organizationId;
+        _organizationName = organizationName;
+        _volume = volume;
+        _toxicPercentage = toxicPercentage;
+    }
+
+    public float GetVolume()
+    {
+        return _volume;
+    }
+
+    private void SetVolume(float volume)
+    {
+        _volume = volume;
+    }
+
+    public float GetToxicPercentage()
+    {
+        return _toxicPercentage;
+    }
+
+    private void SetToxicPercentage(float percentage)
+    {
+        _toxicPercentage = percentage;
     }
 }

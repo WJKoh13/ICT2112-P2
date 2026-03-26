03347736ecef10099e6135614dc69d899461b093
Jolie Ngai <92519643+JolieNgai@users.noreply.github.com>
2026-03-26 20:28:32 +0800

    Imlpemented strategies

--- Domain/Module3/P2-5/Strategy/StaffFootprintStrategy.cs
+++ Domain/Module3/P2-5/Strategy/StaffFootprintStrategy.cs
@@ -4,8 +4,8 @@ namespace ProRental.Domain.Entities.Module3;
 
 public class StaffFootprintStrategy : ICalculateCarbonStrategy
 {
-    public float CalculateFootprint(float data)
+    public double CalculateFootprint(params double[] values)
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Staff footprint strategy has not been implemented yet.");
     }
 }
